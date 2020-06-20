85811a32a513d62ae550c18fa07878eb89cbb982
Matt Logan <mlogan5212@gmail.com>
2020-06-20 13:53:03 -0500

    GET req to db to GET mushrooms

---
+++ src/javascripts/helpers/data/mushroomData.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const baseURL = apiKeys.firebaseConfig.databaseURL;
+const getMushrooms = () => axios.get(`${baseURL}/mushrooms.json`);
+
+export default { getMushrooms };
