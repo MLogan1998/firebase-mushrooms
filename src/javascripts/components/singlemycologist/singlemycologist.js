5c4f4e613eb092aa72977218d6f20fdffe576954
Matt Logan <mlogan5212@gmail.com>
2020-06-23 18:07:45 -0500

    print single myco to dom on click

--- src/javascripts/components/singlemycologist/singlemycologist.js
+++ src/javascripts/components/singlemycologist/singlemycologist.js
@@ -1,4 +1,4 @@
-// import utils from '../../helpers/utils';
+import utils from '../../helpers/utils';
 import mycodata from '../../helpers/data/mycologistData';
 
 const buildMycologist = (e) => {
@@ -6,7 +6,17 @@ const buildMycologist = (e) => {
   mycodata.getMycologistById(mycologistId)
     .then((response) => {
       const mycologist = response.data;
-      console.warn(mycologist);
+      const domString = `
+        <h2 class="text-center">Featured Mycologist</h2>
+        <div class="col-12">
+          <div class="card text-white bg-dark border-0 rounded-0">
+            <div class="card-header">Mycologist ${mycologist.name} (Age: ${mycologist.age})</div>
+            <div class="card-body"></div>
+          </div>
+        </div>
+      `;
+
+      utils.printToDom('#single-myco', domString);
     })
     .catch((err) => console.error(err));
 };
