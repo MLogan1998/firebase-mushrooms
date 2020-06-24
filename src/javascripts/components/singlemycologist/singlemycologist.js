6722c0a287b456de40d223b62e91ea0467dd25e0
Matt Logan <mlogan5212@gmail.com>
2020-06-23 20:43:23 -0500

    delete mushrooms, print syngle myco to dom, smash function

--- src/javascripts/components/singlemycologist/singlemycologist.js
+++ src/javascripts/components/singlemycologist/singlemycologist.js
@@ -1,24 +1,33 @@
 import utils from '../../helpers/utils';
-import mycodata from '../../helpers/data/mycologistData';
+import smash from '../../helpers/smash';
 
 const buildMycologist = (e) => {
   const mycologistId = e.target.closest('.card').id;
-  mycodata.getMycologistById(mycologistId)
-    .then((response) => {
-      const mycologist = response.data;
-      const domString = `
+  smash.getSingleMycoWithShrooms(mycologistId)
+    .then((mycologist) => {
+      console.warn(mycologist);
+      let domString = `
         <h2 class="text-center">Featured Mycologist</h2>
         <div class="col-12">
           <div class="card text-white bg-dark border-0 rounded-0">
             <div class="card-header">Mycologist ${mycologist.name} (Age: ${mycologist.age})</div>
             <div class="card-body"></div>
+            <div class="card-body">
+            <h3 class="card-title">Mushroom(s) Owned:</h3>
+      `;
+
+      mycologist.mushrooms.forEach((mushroom) => {
+        domString += `<p class="card-text">${mushroom.name}</p>`;
+      });
+
+      domString += `
+            </div>
           </div>
         </div>
       `;
-
       utils.printToDom('#single-myco', domString);
     })
-    .catch((err) => console.error(err));
+    .catch((err) => console.error('problem with single mycologist', err));
 };
 
 export default { buildMycologist };
